da9cfdcfa94cc826a3eff5c9bb372ea9aab11919
Pierre Verkest <pierreverkest84@gmail.com>
2024-09-17 18:44:35 +0200

    [IMP] hr_attendance_validation: compute theorical work hours
    Use start/end validation sheet dates to compute theorical time taking care
    of bank holidays

--- hr_attendance_validation/tests/test_hr_attendance_validation.py
+++ hr_attendance_validation/tests/test_hr_attendance_validation.py
@@ -198,6 +198,19 @@ class TestHrAttendanceValidation(TransactionCase):
             }
         )
         self.setup_employee()
+        public_holidays_2021 = self.env["hr.holidays.public"].create(
+            {
+                "year": 2021,
+                "country_id": self.employee.address_id.country_id.id,
+            }
+        )
+        self.env["hr.holidays.public.line"].create(
+            {
+                "name": "Fête nationale",
+                "date": "2021-07-14",
+                "year_id": public_holidays_2021.id,
+            }
+        )
         self.setup_employee_allocation()
         self.setup_employee_holidays()
         self.setup_employee_remote_days()
@@ -558,3 +571,14 @@ class TestHrAttendanceValidation(TransactionCase):
             self.assertEqual(self.employee.hours_current_week, 0)
             self.assertEqual(self.employee.hours_last_month, 13.5)
             self.assertEqual(self.employee.hours_today, 0)
+
+    def test_hr_holidays_public(self):
+        validation = self.HrAttendanceValidation.create(
+            {
+                "employee_id": self.employee.id,
+                "date_from": "2021-07-12",
+                "date_to": "2021-07-18",
+            }
+        )
+
+        self.assertEqual(validation.theoretical_hours, 40 - 8)
